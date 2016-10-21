c25b33cf2d4f1c32840d6b2501b0c439ca95af3b
Ashwith Jerome Rego <ashwith@gmail.com>
2016-10-21 20:00:36 +0530

    Added animation, moved source.
    1. Added a function to generate animations.
    2. Source code moved to source directory.

--- source/vanderpol.py
+++ source/vanderpol.py
@@ -1,5 +1,6 @@
 #!/bin/python3.5
 
+import matplotlib.animation as manimation
 import matplotlib.pyplot as plt
 
 import numpy as np
@@ -34,7 +35,7 @@ def solve_no_init():
     """Solves van Der Pol's equation for the case with no initial
     conditions."""
 
-    time = np.arange(0, 40, 100e-3)  # For 100 FPS animation.
+    time = np.arange(0, 40, 100e-3)
     x_init = [0, 0]
     eps = 0
     return (time, solve_van_der_pol(eps, x_init, time))
@@ -44,7 +45,7 @@ def solve_first_0():
     """Solves van Der Pol's equation for the case with first initial
     condition as zero."""
 
-    time = np.arange(0, 40, 100e-3)  # For 100 FPS animation.
+    time = np.arange(0, 40, 100e-3)
     x_init = [0, 1]
     eps = 0
     return (time, solve_van_der_pol(eps, x_init, time))
@@ -54,7 +55,7 @@ def solve_second_0():
     """Solves van Der Pol's equation for the case with second initial
     condition as zero."""
 
-    time = np.arange(0, 40, 100e-3)  # For 100 FPS animation.
+    time = np.arange(0, 40, 100e-3)
     x_init = [1, 0]
     eps = 0
     return (time, solve_van_der_pol(eps, x_init, time))
@@ -64,7 +65,7 @@ def solve_both_nonzero():
     """Solves van Der Pol's equation for the case with both initial
     conditions as non-zero."""
 
-    time = np.arange(0, 10, 100e-3)  # For 100 FPS animation.
+    time = np.arange(0, 40, 100e-3)
     x_init = [1, 1]
     eps = 0
     return (time, solve_van_der_pol(eps, x_init, time))
@@ -74,7 +75,7 @@ def solve_both_nonzero():
 def solve_eps_0():
     """Solves the van Der Pol ODE for epsilon = 0."""
 
-    time = np.arange(0, 40, 100e-3)  # For 100 FPS animation.
+    time = np.arange(0, 40, 100e-3)
     x_init = [1, 1]
     eps = 0
     return (time, solve_van_der_pol(eps, x_init, time))
@@ -84,7 +85,7 @@ def solve_eps_0_1():
     """Solves the van Der Pol ODE for epsilon = 0.5 (i.e. between 0
     and 1)."""
 
-    time = np.arange(0, 40, 100e-3)  # For 100 FPS animation.
+    time = np.arange(0, 40, 100e-3)
     x_init = [1, 1]
     eps = 0.5
     return (time, solve_van_der_pol(eps, x_init, time))
@@ -93,7 +94,7 @@ def solve_eps_0_1():
 def solve_eps_1():
     """Solves the van Der Pol ODE for epsilon = 1."""
 
-    time = np.arange(0, 40, 100e-3)  # For 100 FPS animation.
+    time = np.arange(0, 40, 100e-3)
     x_init = [1, 1]
     eps = 1
     return (time, solve_van_der_pol(eps, x_init, time))
@@ -103,7 +104,7 @@ def solve_eps_5():
     """Solves the van Der Pol ODE for epsilon = 5. (i.e. greater than
     1)"""
 
-    time = np.arange(0, 40, 100e-3)  # For 100 FPS animation.
+    time = np.arange(0, 40, 100e-3)
     x_init = [1, 1]
     eps = 5
     return (time, solve_van_der_pol(eps, x_init, time))
@@ -122,22 +123,19 @@ class GlobalPlotParams:
 
 # Plot generator function.
 def gen_plt(solve_funcs, title, xlabel, ylabel, legend, out_file):
-    plt.figure()
+    plt.figure(figsize=GlobalPlotParams.fig_size)
 
     for func, marker, color in zip(solve_funcs,
                                    GlobalPlotParams.markers,
                                    GlobalPlotParams.colors):
         time, data = func()
-        #print("DBG:", marker, color)
-        #print(time)
-        #print(data)
-        plt.plot(time, data[:,0], marker=marker, color=color,
+        plt.plot(time, data[:, 0], marker=marker, color=color,
                  linewidth=GlobalPlotParams.linewidth)
 
-    plt.legend(legend, loc='best')
-    plt.title(title)
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
+    plt.legend(legend, loc='best', fontsize=GlobalPlotParams.font_size)
+    plt.title(title, fontsize=GlobalPlotParams.font_size)
+    plt.xlabel(xlabel, fontsize=GlobalPlotParams.font_size)
+    plt.ylabel(ylabel, fontsize=GlobalPlotParams.font_size)
     plt.tight_layout()
     plt.savefig(out_file, format=GlobalPlotParams.ftype)
 
@@ -157,7 +155,7 @@ def gen_plts_init_cond():
             xlabel="Time",
             ylabel="Amplitude",
             legend=legend,
-            out_file="output/van_der_pol_init.eps")
+            out_file="../output/van_der_pol_init.eps")
 
 
 # Generate plot showing variation in damping factor.
@@ -171,7 +169,7 @@ def gen_plt_eps_var():
               "Damping Factor: $\epsilon = 5$"]
 
     gen_plt(ode_cases, "Variation of Damping Factor", "Time",
-            "Amplitude", legend, "output/van_der_pol_eps.eps")
+            "Amplitude", legend, "../output/van_der_pol_eps.eps")
 
 
 # Generate plots for all the cases above
@@ -179,5 +177,36 @@ def gen_all_plots():
     gen_plts_init_cond()
     gen_plt_eps_var()
 
+
+def gen_video(filename):
+    FFMpegWriter = manimation.writers['ffmpeg']
+    metadata = dict(title='van Der Pol Oscillator', artist='Matplotlib',
+                    comment="An animation of the van Der Pol\
+                    oscillator's amplitude with time.")
+    writer = FFMpegWriter(fps=60, metadata=metadata, codec='libtheora')
+
+    eps = 2
+    x_init = [1, 1]
+    time = np.arange(0, 40, 100e-3)
+    amp = solve_van_der_pol(eps, x_init, time)[:, 0]
+
+    fig = plt.figure(figsize=(19.20, 10.80))
+
+    with writer.saving(fig, filename, 100):
+        for idx in range(len(time)):
+            plt.cla()
+            plt.xlim(0, 40)
+            plt.ylim(amp.min() - 1, amp.max() + 1)
+            plt.xlabel('Time (seconds)', fontsize=20)
+            plt.ylabel('Amplitude', fontsize=25)
+            plt.title('van Der Pol Oscillator (Animation at 6x Speed)',
+                      fontsize=20)
+            plt.plot(time[idx], amp[idx], 'bo')
+            plt.plot(time[:idx], amp[:idx], linewidth=2, color='blue')
+            plt.grid()
+            writer.grab_frame()
+
+
 if __name__ == '__main__':
     gen_all_plots()
+    gen_video('../output/animation.ogg')
